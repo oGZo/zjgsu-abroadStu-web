9a65d86613f120ed556de3ac2731d19305cd5ded
sanqi.zgz <sanqi.zgz@120yibao.com>
2016-05-11 23:43:49 +0800

    登录状态下默认进入该角色第一个模块

--- app/scripts/controllers/LayoutViewController.js
+++ app/scripts/controllers/LayoutViewController.js
@@ -9,6 +9,13 @@ define(['app'], function (app) {
             if (!$cookieStore.get(YB.param.sysParam.token)) {
                 $location.path('/login');
                 return;
+            }else{
+                var moduleJson = {
+                    1 : 'student',
+                    2 : 'teacher',
+                    3 : 'manage'
+                };
+                $location.path('/'+moduleJson[YB.localStorage.getItem('userType')]);
             }
             var loadingObj = $('.loading-area');
             loadingObj.fadeOut();
